5629e36fbaf8debcd7557eb25894bda6a63a65a8
Debarshi Ray <debarshir@gnome.org>
2016-01-08 18:33:25 +0100

    lokview: Use GVfs URIs to open documents
    As far as I can see, LibreOfficeKit is able to handle GVfs URIs just
    fine. This means that we can support remote filesystems mounted by GVfs
    (like ownCloud).
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753686

--- src/lokview.js
+++ src/lokview.js
@@ -168,9 +168,8 @@ const LOKView = new Lang.Class({
 
     _onLoadFinished: function(manager, doc, docModel) {
         if (docModel == null && doc != null) {
-            let [ location, ] = GLib.filename_from_uri (doc.uri);
             this._doc = doc;
-            this.view.open_document(location, "{}", null, Lang.bind(this, this.open_document_cb));
+            this.view.open_document(doc.uri, "{}", null, Lang.bind(this, this.open_document_cb));
             this._progressBar.show();
         }
     },
