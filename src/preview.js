isabella232/gnome-documents

commit 34d7c0abe97276401e2c360276cfdfb88abee077

books: Hide the "Present" menu item in Books
We're unlikely to make presentations with files from Books, so remove
the menu item.

https://bugzilla.gnome.org/show_bug.cgi?id=767083

src/preview.js
@@ -146,8 +146,10 @@ const PreviewView = new Lang.Class({
             Lang.bind(this, this._updateNightMode));
 
         this._togglePresentation = Application.application.lookup_action('present-current');
-        let presentCurrentId = Application.application.connect('action-state-changed::present-current',
-            Lang.bind(this, this._onPresentStateChanged));
+        if (!Application.application.isBooks) {
+            let presentCurrentId = Application.application.connect('action-state-changed::present-current',
+                Lang.bind(this, this._onPresentStateChanged));
+        }
 
         Application.documentManager.connect('load-started',
                                             Lang.bind(this, this._onLoadStarted));
@@ -165,7 +167,8 @@ const PreviewView = new Lang.Class({
                 rotLeft.disconnect(rotLeftId);
                 rotRight.disconnect(rotRightId);
                 this._places.disconnect(placesId);
-                Application.application.disconnect(presentCurrentId);
+                if (!Application.application.isBooks)
+                    Application.application.disconnect(presentCurrentId);
                 Application.application.disconnect(nightModeId);
             }));
     },
@@ -560,7 +563,8 @@ const PreviewView = new Lang.Class({
             this.view.set_model(this._model);
             this._navControls.setModel(model);
             this._navControls.show();
-            this._togglePresentation.enabled = true;
+            if (this._togglePresentation)
+                this._togglePresentation.enabled = true;
 
             if (Application.documentManager.metadata)
                 this._bookmarks = new GdPrivate.Bookmarks({ metadata: Application.documentManager.metadata });

data/ui/preview-menu.ui
@@ -16,6 +16,7 @@
       </item>
       <item>
         <attribute name="action">app.present-current</attribute>
+        <attribute name='hidden-when'>action-missing</attribute>
         <attribute name="label" translatable="yes">Present</attribute>
         <attribute name="accel">F5</attribute>
       </item>

src/application.js
@@ -573,11 +573,6 @@ const Application = new Lang.Class({
             { name: 'edit-current' },
             { name: 'view-current',
               window_mode: WindowMode.WindowMode.EDIT },
-            { name: 'present-current',
-              window_mode: WindowMode.WindowMode.PREVIEW,
-              callback: this._onActionToggle,
-              state: GLib.Variant.new('b', false),
-              accels: ['F5'] },
             { name: 'print-current', accels: ['<Primary>p'],
               callback: this._onActionPrintCurrent },
             { name: 'search',
@@ -641,11 +636,18 @@ const Application = new Lang.Class({
                              WindowMode.WindowMode.SEARCH] }
         ];
 
+        if (!this.isBooks) {
+            this._actionEntries.push (
+            { name: 'present-current',
+              window_mode: WindowMode.WindowMode.PREVIEW,
+              callback: this._onActionToggle,
+              state: GLib.Variant.new('b', false),
+              accels: ['F5'] });
+            this._initGettingStarted();
+        }
+
         this._initActions();
         this._initAppMenu();
-
-        if (!this.isBooks)
-            this._initGettingStarted();
     },
 
     _createWindow: function() {

commit a399a86cedd4b67aa5aaec695eeceacbc12d5ad8

preview: Increase scope of presentCurrentId
Fixes bug introduced in commit 34d7c0a

https://bugzilla.gnome.org/show_bug.cgi?id=767083

src/preview.js
@@ -147,7 +147,7 @@ const PreviewView = new Lang.Class({
 
         this._togglePresentation = Application.application.lookup_action('present-current');
         if (!Application.application.isBooks) {
-            let presentCurrentId = Application.application.connect('action-state-changed::present-current',
+            var presentCurrentId = Application.application.connect('action-state-changed::present-current',
                 Lang.bind(this, this._onPresentStateChanged));
         }
 