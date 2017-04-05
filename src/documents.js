aeec467b70b97294b26a7c7c150c108ab50b0192
Debarshi Ray <debarshir@gnome.org>
2017-04-05 18:15:25 +0200

    documents: Use a global named constant for the ownCloud prefix
    https://bugzilla.gnome.org/show_bug.cgi?id=781032

--- src/documents.js
+++ src/documents.js
@@ -1102,6 +1102,8 @@ const GoogleDocument = new Lang.Class({
     }
 });
 
+const OWNCLOUD_PREFIX = 'owncloud:';
+
 const OwncloudDocument = new Lang.Class({
     Name: 'OwncloudDocument',
     Extends: DocCommon,
@@ -1410,7 +1412,7 @@ const DocumentManager = new Lang.Class({
 
     _identifierIsOwncloud: function(identifier) {
         return (identifier &&
-                (identifier.indexOf('owncloud:') != -1));
+                (identifier.indexOf(OWNCLOUD_PREFIX) != -1));
     },
 
     _identifierIsSkydrive: function(identifier) {
