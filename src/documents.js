isabella232/gnome-documents

commit 4133adf06a0ff473e6234f8ab3eb88c47589e627

documents: Let everybody provide their own download implementation
This is meant to reduce our reliance on GdPdfLoader for loading remote
documents. We can open ODFs and OOXMLs in LOKDocView, so we don't need
to convert everything to PDFs. In the future, remote sub-classes will
provider their own implementation to download a document, and this will
help unify the loading across the various sub-classes.

https://bugzilla.gnome.org/show_bug.cgi?id=774937

src/documents.js
@@ -365,6 +365,48 @@ const DocCommon = new Lang.Class({
         }
     },
 
+    download: function(useCache, cancellable, callback) {
+        let localFile = Gio.File.new_for_uri(this.uriToLoad);
+        let localPath = localFile.get_path();
+        let localDir = GLib.path_get_dirname(localPath);
+        GLib.mkdir_with_parents(localDir, 448);
+
+        if (!useCache) {
+            this.downloadImpl(localFile, cancellable, callback);
+            return;
+        }
+
+        localFile.query_info_async(Gio.FILE_ATTRIBUTE_TIME_MODIFIED,
+                                   Gio.FileQueryInfoFlags.NONE,
+                                   GLib.PRIORITY_DEFAULT,
+                                   cancellable,
+                                   Lang.bind(this,
+            function(object, res) {
+                let info;
+
+                try {
+                    info = object.query_info_finish(res);
+                } catch (e) {
+                    this.downloadImpl(localFile, cancellable, callback);
+                    return;
+                }
+
+                let cacheMtime = info.get_attribute_uint64(Gio.FILE_ATTRIBUTE_TIME_MODIFIED);
+                cacheMtime /= 1000000;
+
+                if (this.mtime <= cacheMtime) {
+                    callback(true, null);
+                    return;
+                }
+
+                this.downloadImpl(localFile, cancellable, callback);
+            }));
+    },
+
+    downloadImpl: function(localFile, cancellable, callback) {
+        throw(new Error('DocCommon implementations must override downloadImpl'));
+    },
+
     load: function() {
         log('Error: DocCommon implementations must override load');
     },

commit ab56c5c37d7833ae2f8fa57f1f63fa048c959ab4

documents: Fix the retrieval of the cache's modification time
Gio.FILE_ATTRIBUTE_TIME_MODIFIED returns a value in seconds, not
microseconds.

Fallout from 4133adf06a0ff473e6234f8ab3eb88c47589e627

https://bugzilla.gnome.org/show_bug.cgi?id=774937

src/documents.js
@@ -392,8 +392,6 @@ const DocCommon = new Lang.Class({
                 }
 
                 let cacheMtime = info.get_attribute_uint64(Gio.FILE_ATTRIBUTE_TIME_MODIFIED);
-                cacheMtime /= 1000000;
-
                 if (this.mtime <= cacheMtime) {
                     callback(true, null);
                     return;