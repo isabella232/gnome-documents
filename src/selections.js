isabella232/gnome-documents

commit 70381b8723dc491f3267fc804178b9d64beedd2d

selections: Enable printing only for documents handled by EvView
Now that we don't convert everything to PDFs, we might not be able to
print everything that's not a collection. eg., we can't print EPUBs,
ODFs and OOXMLs. Therefore it is not enough to only check for
collecitons.

https://bugzilla.gnome.org/show_bug.cgi?id=781533

src/selections.js
@@ -942,7 +942,7 @@ const SelectionToolbar = new Lang.Class({
         let hasSelection = (selection.length > 0);
 
         let showTrash = hasSelection;
-        let showPrint = hasSelection;
+        let showPrint = false;
         let showProperties = hasSelection;
         let showOpen = hasSelection;
         let showShare = hasSelection;
@@ -963,15 +963,21 @@ const SelectionToolbar = new Lang.Class({
                     showShare = false;
 
                 showTrash &= doc.canTrash();
-                showPrint &= !doc.collection;
             }));
 
         showOpen = (apps.length > 0);
 
-        if (selection.length > 1) {
-            showPrint = false;
-            showProperties = false;
+        if (selection.length == 1) {
+            let doc = Application.documentManager.getItemById(selection[0]);
+            doc.load(null, null, Lang.bind(this,
+                function(doc, docModel, error) {
+                    showPrint = doc.canPrint(docModel);
+                    this._toolbarPrint.set_sensitive(showPrint);
+                }));
         }
+
+        if (selection.length > 1)
+            showProperties = false;
 
         let openLabel = null;
         if (apps.length == 1) {

commit b51b13ffd9f1f878ff290afe05afcc386989bb85

selections: Don't attempt to load collections
Attempting to load a collection is a programming error. Currently, that
leads to a GError and a NULL EvDocument pointer being passed to the
callback, which ends up getting handled properly. It's better to avoid
even attempting the load, in case such programming errors are dealt
with more strictly in the future.

Fallout from 70381b8723dc491f3267fc804178b9d64beedd2d

https://gitlab.gnome.org/GNOME/gnome-documents/issues/7

src/selections.js
@@ -969,11 +969,13 @@ var SelectionToolbar = new Lang.Class({
 
         if (selection.length == 1) {
             let doc = Application.documentManager.getItemById(selection[0]);
-            doc.load(null, null, Lang.bind(this,
-                function(doc, docModel, error) {
-                    showPrint = doc.canPrint(docModel);
-                    this._toolbarPrint.set_sensitive(showPrint);
-                }));
+            if (!doc.collection) {
+                doc.load(null, null, Lang.bind(this,
+                    function(doc, docModel, error) {
+                        showPrint = doc.canPrint(docModel);
+                        this._toolbarPrint.set_sensitive(showPrint);
+                    }));
+            }
         }
 
         if (selection.length > 1)